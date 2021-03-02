e8c413171240c4c2f3e8a3eef9cc8b1c9a811dc0
Robert Głuszkiewicz <robert@macbookpro.home>
2021-03-03 00:14:45 +0100

    Added 19.2 task

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Task, Long> {
 
+    @Override
     List<Task> findAll();
+
+    @Override
+    Optional<Task> findById(Long id);
 }
